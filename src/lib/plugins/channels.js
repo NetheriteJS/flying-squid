02c20cefbce992afaf0966d5853f463d7b6a7e41
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-01-21 18:31:13 +0200

    Update channels.js

--- src/lib/plugins/channels.js
+++ src/lib/plugins/channels.js
@@ -1,7 +1,7 @@
 const PLAY = require('minecraft-protocol').states.PLAY
 
 module.exports.player = (player) => {
-  player.sendBrand = async (brand = 'flying-squid') => {
+  player.sendBrand = async (brand = 'NetheriteJS') => {
     if (player._client.state !== PLAY) throw new Error(`The state of the player must be PLAY (actual state: ${player._client.state})`)
     player._client.writeChannel((
       player._client.protocolVersion >= 385 // (385 = 1.13-pre3) as of 1.13 (The Flattening), the name of the default channels has changed
